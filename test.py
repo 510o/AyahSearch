80fcaf13964181ddf5f371ee68d5668c1361936d
510o <1510oxzso@gmail.com>
2026-08-08 17:46:29 +0300

    Optional Reshape

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
-from arabicreshaper import reshape # pip install git+https://github.com/510o/ArabicReshaper.git
 from searcher.segmenter import split_by_letters
 from main import plain_engine
+
+RESHAPE = True  # set to False if you don't want to reshape the text for display
+
+if RESHAPE:
+    from arabicreshaper import reshape # pip install git+https://github.com/510o/ArabicReshaper.git
 
 print("Ayah Search - search by letter, diacritice, or numbers")
 
@@ -16,14 +20,14 @@ while True:
     verses = results.get("verses", {})
     n = len(verses)
 
-    for i, chunk in enumerate(chunks):
-        if chunk[0] in plain_engine.letters:
-            chunks[i] = reshape(chunk)[::-1]
+    if RESHAPE:
+        for i, chunk in enumerate(chunks):
+            if chunk[0] in plain_engine.letters:
+                chunks[i] = reshape(chunk)[::-1]
 
     print(f"\n{repr(chunks)} {n} results:")
 
     for (sura, aya), text in verses.items():
-        shaped = reshape(text)[::-1]
-        print(f"[{sura}:{aya}] {shaped}")
+        print(f"[{sura}:{aya}] {reshape(text)[::-1] if RESHAPE else text}")
 
     print("-" * 40)
