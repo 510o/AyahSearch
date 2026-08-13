0c9a47426c141caf4ad4d789717cd07d821c4b95
510o <1510oxzso@gmail.com>
2026-08-14 00:04:33 +0300

    Just install python-bidi

--- test.py
+++ test.py
@@ -6,25 +6,6 @@ RESHAPE = True  # set to False if you don't want to reshape the text for display
 
 if RESHAPE:
     from arabicdisplayer import reshape, line_breaker, apply_display, align_text # pip install git+https://github.com/510o/ArabicDisplayer.git
-    def formation(text: str, head: str = "", width: int = 0):
-        if width:
-            text = line_breaker(text, width)
-
-
-        text = apply_display(text)
-
-        if head and text.count('░') == len(head):
-            head = iter(head)
-            text = ''.join(
-                next(head) if c == '░' else c
-                for c in text
-            )
-
-        if width:
-            text = align_text(text, width)
-
-        return text
-
 
 print(line_breaker("Ayah Search - search by letters, diacritices, or numbers", get_terminal_size().columns))
 
@@ -44,20 +25,20 @@ while True:
     if RESHAPE:
         for i, chunk in enumerate(chunks):
             if chunk[0] in plain_engine.letters:
-                chunks[i] = apply_display(reshape(chunk))
+                chunks[i] = reshape(chunk)
 
-        print(formation(f"\n{'░' * len(repr(chunks))} {reshape("نتائج البحث")} {'░' * len(str(n))}░", f":{n}{chunks}", width))
+        head = f"{chunks} نتائج البحث {n}:"
+        print(align_text(apply_display(line_breaker(reshape(head), width)), width))
+        
     else:
-        print(f"\n:نتائج البحث {n} {repr(chunks)}")
+        print(head)
 
     for (sura, aya), text in verses.items():
-        lverse = f"[{sura}: {aya}] "
-        verse_len = len(lverse)
-
+        verse = f"[{sura}: {aya}] {text}"
         if RESHAPE:
-            print(formation('░' * verse_len + reshape(text), f" [{aya} :{sura}]", width))
+            print(align_text(apply_display(line_breaker(reshape(verse), width)), width))
 
         else:
-            print(lverse + text)
+            print(verse)
 
     print("-" * width)
