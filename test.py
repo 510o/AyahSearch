eba9b05e4f772e82ab7919c6b2e498a0f0d0ac79
510o <1510oxzso@gmail.com>
2026-08-13 23:17:22 +0300

    Improve text formatting

--- test.py
+++ test.py
@@ -5,7 +5,26 @@ from main import plain_engine
 RESHAPE = True  # set to False if you don't want to reshape the text for display
 
 if RESHAPE:
-    from arabicreshaper import reshape, line_breaker # pip install git+https://github.com/510o/ArabicReshaper.git
+    from arabicdisplayer import reshape, line_breaker, apply_display, align_text # pip install git+https://github.com/510o/ArabicDisplayer.git
+    def formation(text: str, head: str = "", width: int = 0):
+        if width:
+            text = line_breaker(text, width)
+
+
+        text = apply_display(text)
+
+        if head and text.count('░') == len(head):
+            head = iter(head)
+            text = ''.join(
+                next(head) if c == '░' else c
+                for c in text
+            )
+
+        if width:
+            text = align_text(text, width)
+
+        return text
+
 
 print(line_breaker("Ayah Search - search by letters, diacritices, or numbers", get_terminal_size().columns))
 
@@ -25,20 +44,20 @@ while True:
     if RESHAPE:
         for i, chunk in enumerate(chunks):
             if chunk[0] in plain_engine.letters:
-                chunks[i] = reshape(chunk, True)
+                chunks[i] = apply_display(reshape(chunk))
 
-    print(f"\n{repr(chunks)} {n} results:")
+        print(formation(f"\n{'░' * len(repr(chunks))} {reshape("نتائج البحث")} {'░' * len(str(n))}░", f":{n}{chunks}", width))
+    else:
+        print(f"\n:نتائج البحث {n} {repr(chunks)}")
 
     for (sura, aya), text in verses.items():
-        lhead = f"[{sura}: {aya}] "
-        head_len = len(lhead)
-        rhead = iter(f" [{aya} :{sura}]")
+        lverse = f"[{sura}: {aya}] "
+        verse_len = len(lverse)
 
         if RESHAPE:
-            line = reshape(line_breaker("░" * head_len + text, width, True), True)
-            line = ''.join(next(rhead) if c == '░' else c for c in line)
-            print(line)
+            print(formation('░' * verse_len + reshape(text), f" [{aya} :{sura}]", width))
+
         else:
-            print(lhead + text)
+            print(lverse + text)
 
     print("-" * width)
