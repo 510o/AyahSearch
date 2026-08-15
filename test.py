4fa749cbaffb17ddd5c14d4c609ef9e90012b278
510o <1510oxzso@gmail.com>
2026-08-15 14:09:52 +0300

    Again with simplicity

--- test.py
+++ test.py
@@ -6,20 +6,6 @@ RESHAPE = True  # set to False if you don't want to reshape the text for display
 
 if RESHAPE:
     from arabicdisplayer import reshape, line_breaker, apply_display, align_text # pip install git+https://github.com/510o/ArabicDisplayer.git
-    from unicodedata import combining
-    def build_header(body, prefix="", suffix="", width=0, right_alignment=False):
-        vlen = lambda line: sum(not combining(c) for c in line)
-
-        body = reshape(body)
-        if width:
-            offset = min(vlen(prefix), width)
-            body = line_breaker(body, width, first_line_offset=offset)
-        lines = apply_display(body).split('\n')
-
-        lines[0] = lines[0] + prefix
-        lines[-1] = suffix + lines[-1]
-
-        return align_text('\n'.join(lines), width, right_alignment) if width else '\n'.join(lines)
 
 print(line_breaker("Ayah Search - search by letters, diacritices, or numbers", get_terminal_size().columns))
 
@@ -36,25 +22,23 @@ while True:
     verses = results.get("verses", {})
     n = len(verses)
 
-    print('\n')
     if RESHAPE:
         for i, chunk in enumerate(chunks):
             if chunk[0] in plain_engine.letters:
-                chunks[i] = apply_display(reshape(chunk))
+                chunks[i] = reshape(chunk)
 
         head = f"{chunks} نتائج البحث {n}:"
-        
-        print(build_header("نتائج البحث", f" {chunks}", f":{n} ", width, True))
+        print(align_text(apply_display(line_breaker(reshape(head), width)), width))
         
     else:
         print(head)
 
     for (sura, aya), text in verses.items():
+        verse = f"[{sura}: {aya}] {text}"
         if RESHAPE:
-            head = f" [{aya} :{sura}]"
-            print(build_header(text, head, "", width, True))
+            print(align_text(apply_display(line_breaker(reshape(verse), width)), width))
 
         else:
-            print(f"[{sura}: {aya}] {text}")
+            print(verse)
 
     print("-" * width)
